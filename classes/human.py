f7120ff30c86d566a5e0d022e443900c00faef45
klit_ma <margrit.klitz@dlr.de>
2020-03-21 11:59:53 -0700

    Added function to load images

--- classes/human.py
+++ classes/human.py
@@ -1,7 +1,18 @@
 import pygame
+import os
 import random
 import numpy as np
 
+_image_library = {}
+def get_image(path):
+        global _image_library
+        image = _image_library.get(path)
+        if image == None:
+                canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+                image = pygame.image.load(canonicalized_path)
+                _image_library[path] = image
+        return image
+        
 class human(object):
     #define position of the human,  and the current movement.
     #draw a cricle representing the human.
