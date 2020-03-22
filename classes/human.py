7351f68726acb23c8348cb1183c2aeafe3526bae
klit_ma <margrit.klitz@dlr.de>
2020-03-22 02:25:19 -0700

    Changed some pics

--- classes/human.py
+++ classes/human.py
@@ -87,8 +87,8 @@ class human(object):
         imgcode = {'well': 'healthy.png',
                    'infected': 'infected.png',
                    'ill': 'infected2.png',
-                   'recovered': 'recovered.png',
-                   'dead': 'dead.png'
+                   'recovered': 'recovered3.png',
+                   'dead': 'dead2.png'
                    }
 
         self.img = pygame.transform.scale(get_image(imgcode[self.state]), (20, 20))
